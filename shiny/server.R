6e0dd51ccad75407c0429410277447061d343a81
Kevin Siswandi  (NCS) <kevinsis@ncs.com.sg>
2016-04-25 01:23:22 +0800

    Modified server and ui

--- shiny/server.R
+++ shiny/server.R
@@ -2,10 +2,12 @@ library(shiny)
 library(NLP)
 library(tm)
 
-predict.ngram <- function(pattern) {
-  matching <- grep(pattern, trigrams)
-  if(length(matching) < 5)
-    top5 <- sort(table(matching), decreasing=T)[1:length(matching)]
+predict.ngram <- function(pattern, trigrams) {
+  matching <- grep(pattern, trigrams, ignore.case=TRUE)
+  if(length(matching) == 0)
+      top5 <- 'the'
+  else if(length(matching) < 5)
+      top5 <- sort(table(matching), decreasing=T)[1:length(matching)]
   else
     top5 <- sort(table(matching), decreasing=T)[1:5]
   
@@ -16,26 +18,30 @@ shinyServer(
   function(input, output){
     output$value <- renderPrint({ input$text })
     output$prediction <- renderPrint({
-      withProgress(message='Computing for the next word...', value=0, {
+        if(input$goButton == 1){
+            withProgress(message='Computing the next word:', value=0, {
+                
+                incProgress(0.25, detail='Loading text files...')
+                tweet.con <- file("en_US.twitter.txt", "r")
+                combined.data <- readLines(tweet.con)
+                close(tweet.con)
+                
+                incProgress(0.25, detail = 'Preprocessing corpus...')
+                combined.data <- removePunctuation(combined.data)
+                combined.data <- tolower(combined.data)
+                combined.data <- stripWhitespace(combined.data)
+                
+                incProgress(0.25, detail='Creating 3-grams...')
+                combined.data <- strsplit(combined.data, " ", fixed=TRUE)
+                trigrams <- sapply(combined.data, ngrams, 3L)
+                trigrams <- vapply(trigrams, paste, "", collapse=" ")
+                
+                incProgress(0.25, detail='Getting your top suggestions...')
+                x <- predict.ngram(input$text, trigrams)
+            })
+            x[1]
+        }
         
-        incProgress(0.1, detail='Loading text files...')
-        tweet.con <- file("en_US.twitter.txt", "r")
-        combined.data <- readLines(tweet.con)
-        close(tweet.con)
-        
-        incProgress(0.1, detail = 'Preprocessing corpus...')
-        combined.data <- removePunctuation(combined.data)
-        combined.data <- tolower(combined.data)
-        combined.data <- stripWhitespace(combined.data)
-        
-        incProgress(0.1, detail='Creating 3-grams...')
-        combined.data <- strsplit(combined.data, " ", fixed=TRUE)
-        trigrams <- sapply(combined.data, ngrams, 3L)
-        trigrams <- vapply(trigrams, paste, "", collapse=" ")
-        
-        incProgress(0.1, detail='Getting your top suggestions...')
-        predict.ngram(input$text)[1]
       })
       })
-  }
-)
+  
--- shiny/ui.R
+++ shiny/ui.R
@@ -4,11 +4,15 @@ shinyUI(fluidPage(
   titlePanel('Coursera Capstone: Next-word Prediction App'),
   
   fluidRow(
-    column(3, textInput("text", label=h3('Text Input'), value='Enter Text...')),
+    column(3, textInput("text", label=h3('Text Input'), value='Enter a sentence...')),
     column(3, h4('Next-word suggestion:'), verbatimTextOutput("prediction"))
   ),
   
   hr(),
-  fluidRow(column(3, verbatimTextOutput("value")))
-  
+  fluidRow(
+      column(3, verbatimTextOutput('value'))
+  ),
+  actionButton("goButton", "Predict!"),
+  fluidRow(column(3, h4('Please be patient after hitting the button as the text preprocessing is slow, ~10mins')))
+
 ))
