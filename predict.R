e4cd239c08125417caef19b71a285a97b679b8af
Kevin Siswandi <kevinsiswandi@gmail.com>
2016-06-02 17:27:14 +0800

    Add files via upload

--- predict.R
+++ predict.R
@@ -1,24 +1,29 @@
 df_trigram <- read.csv("df_trigram_final.csv", stringsAsFactors = FALSE)
-df_trigram$word1 <- as.character(df_trigram$word1)
-df_trigram$word2 <- as.character(df_trigram$word2)
-df_trigram$word3 <- as.character(df_trigram$word3)
+
+# myCorpus <- PCorpus(DirSource("training_final", encoding = 'UTF-8', mode='text'),
+#                     dbControl=list(dbName="myCorpus.db", dbType="DB1"))
+
+# df_trigram$word1 <- as.character(df_trigram$word1)
+# df_trigram$word2 <- as.character(df_trigram$word2)
+# df_trigram$word3 <- as.character(df_trigram$word3)
 
 predict_word <- function(x) {
   
   x <- tolower(x)
+  x <- gsub("[^[:alnum:][:space:]]", " ", x)
   splitted <- unlist(strsplit(x, split=" "))
   
   N <- length(splitted)
   
-  subdata1 <- df_trigram[grep(splitted[N], df_trigram$word2),]
-  subdata2 <- subdata1[grep(splitted[N-1], subdata1$word1),]
+  subdata1 <- df_trigram[df_trigram$word2 == splitted[N],]
+  subdata2 <- subdata1[subdata1$word1 == splitted[N-1],]
   
   if(nrow(subdata2) > 0)
-    predicted <- subdata2[which.max(subdata2$count), "word3"]
+    predicted <- subdata2[order(subdata2$count, decreasing=TRUE), "word3"]
   else if (nrow(subdata1 > 0))
-    predicted <- subdata1[which.max(subdata1$count), "word3"]
+    predicted <- subdata1[order(subdata1$count, decreasing=TRUE), "word3"]
   else
     predicted <- 'kimbek'
   
-  predicted
+  predicted[1:3]
 }
