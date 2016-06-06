ef910b01b17cf0bd9419a2fdc224b6aaef811c35
Kevin Siswandi <kevinsiswandi@gmail.com>
2016-06-06 10:08:44 +0800

    Fixed the backoff logic

--- predict.R
+++ predict.R
@@ -11,19 +11,32 @@ predict_word <- function(x) {
   
   x <- tolower(x)
   x <- gsub("[^[:alnum:][:space:]]", " ", x)
+  
   splitted <- unlist(strsplit(x, split=" "))
   
   N <- length(splitted)
+  if(N < 2)
+    stop('2 words at least please.')
   
   subdata1 <- df_trigram[df_trigram$word2 == splitted[N],]
   subdata2 <- subdata1[subdata1$word1 == splitted[N-1],]
   
   if(nrow(subdata2) > 0)
     predicted <- subdata2[order(subdata2$count, decreasing=TRUE), "word3"]
-  else if (nrow(subdata1 > 0))
-    predicted <- subdata1[order(subdata1$count, decreasing=TRUE), "word3"]
+  else if (nrow(subdata1) > 0){
+    predicted <- subdata1 %>%
+      group_by(word2, word3) %>%
+      summarise(count=n()) %>%
+      arrange(desc(count))
+    predicted <- predicted$word3
+  }
   else
-    predicted <- 'kimbek'
+    predicted <- 'the'
   
-  predicted[1:3]
+  predicted <- na.omit(predicted)
+  
+  if(length(predicted) >= 3)
+    predicted[1:3]
+  else
+    predicted[1:length(predicted)]
 }
